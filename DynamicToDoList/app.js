7d6e3363d5bcbe172ee4f8f8cdfa85314e911118
SoRa <sonamraisx007@gmail.com>
2024-07-02 19:35:36 +0545

    Dynamic To-Do List added

---
+++ DynamicToDoList/app.js
@@ -0,0 +1,31 @@
+const input = document.getElementById("new-task");
+const addTask = document.getElementById("add-task");
+const taskList = document.getElementById("task-list");
+
+//stylize tasklist
+taskList.style.display = "flex";
+taskList.style.flexDirection = "column";
+taskList.style.gap = "8px";
+
+addTask.addEventListener("click", () => {
+  console.log(input.value);
+  //adding list
+  const newTask = document.createElement("li");
+  newTask.innerText = input.value;
+  //stylize list
+  newTask.style.listStyle = "none";
+  newTask.style.display = "flex";
+  newTask.style.gap = "8px";
+  taskList.appendChild(newTask);
+
+  //adding button
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "Complete";
+  newTask.appendChild(completeButton);
+
+  //add eventlistener to button
+  completeButton.addEventListener("click", () => {
+    newTask.classList.add("completed");
+    // newTask.remove();
+  });
+});
