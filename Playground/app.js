3311dc9c11d2e4af09bca64681d8d976d7fc3223
SoRa <sonamraisx007@gmail.com>
2024-07-02 23:04:53 +0545

    playground added

---
+++ Playground/app.js
@@ -0,0 +1,5 @@
+const contentDiv = document.getElementById("content");
+// contentDiv.innerHTML =
+//   "<image src='https://picsum.photos/200/300' alt='some image' />";
+
+contentDiv.innerText = "Hello World";
